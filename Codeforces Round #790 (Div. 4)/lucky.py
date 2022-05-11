6dc3785e15ff75d9b3842183c0f5dc36930ccfdc
MeasDavicheanin <42645102+MeasDavicheanin@users.noreply.github.com>
2022-05-11 12:02:45 -0600

    Update lucky.py

--- Codeforces Round #790 (Div. 4)/lucky.py
+++ Codeforces Round #790 (Div. 4)/lucky.py
@@ -1,1 +1,11 @@
-
+t=int(input())
+for i in range(0, t):
+    num=input()
+    num1=list(num[:3])
+    num2=list(num[3:])
+    number1=[int(x) for x in num1]
+    number2=[int(x) for x in num2]
+    if sum(number1)==sum(number2):
+        print("YES")
+    else:
+        print("No")
